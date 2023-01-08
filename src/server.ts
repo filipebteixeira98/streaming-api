a562e357d6448b5613057d9caed29547a8205c34
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-01-08 02:50:07 -0300

    chore: created build schema & initialized apollo-server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
 import 'reflect-metadata';
+
+import { buildSchema } from 'type-graphql';
+import { ApolloServer } from 'apollo-server';
+
+import './utils/connection';
+
+async function bootstrap() {
+  const schema = await buildSchema({
+    resolvers: [],
+  });
+
+  const server = new ApolloServer({ schema });
+
+  server.listen({ port: 4100 }, () =>
+    console.log('Server is running at http://localhost:4100')
+  );
+}
+
+bootstrap();
