1be22928dc5ad85f706137d99ec24a34bb96bc18
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-01-08 03:49:12 -0300

    chore: added mutation to create category & defined input types

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,9 +1,17 @@
-import { Query, Resolver } from 'type-graphql';
+import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql';
 
 import Category from './Category';
 
 import CategorySchema from '../../models/CategorySchema';
 
+@InputType()
+class CategoryInput {
+  @Field()
+  description: string;
+
+  @Field()
+  name: string;
+}
 @Resolver(Category)
 class CategoryResolver {
   @Query(() => [Category])
@@ -11,6 +19,13 @@ class CategoryResolver {
     const categories = await CategorySchema.find();
 
     return categories;
+  }
+
+  @Mutation(() => Category)
+  async createCategory(@Arg('categoryInput') categoryInput: CategoryInput) {
+    const category = await CategorySchema.create(categoryInput);
+
+    return category;
   }
 }
 
